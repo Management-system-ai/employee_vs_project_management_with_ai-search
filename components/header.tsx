3093a0e51d0c4b96b0ca05c72d3bcdd41aa3c2b5
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2024-12-31 14:47:58 +0700

    fix format and eslint

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,19 @@
-import Image from "next/image";
-import ProfileIcon from "../resources/images/icons/icon profile.png"
+import Image from 'next/image';
+import ProfileIcon from '../resources/images/icons/icon profile.png';
 export default function Header() {
-    return (
-      <header className="flex items-center justify-between px-6 py-4 bg-gray-100 border-b">
-        <div>
-        <p className="text-[32px] font-bold text-[#212F3F]">Hello, Anna Belle</p>
-        <p className="text-[18px] font-normal">Here’s the current status for today</p>
-        </div>
-        <div className="flex items-center">
-          <Image src={ProfileIcon} alt="profile" height={40} width={40}></Image>
-        </div>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="flex items-center justify-between border-b bg-gray-100 px-6 py-4">
+      <div>
+        <p className="text-[32px] font-bold text-[#212F3F]">
+          Hello, Anna Belle
+        </p>
+        <p className="text-[18px] font-normal">
+          Here’s the current status for today
+        </p>
+      </div>
+      <div className="flex items-center">
+        <Image src={ProfileIcon} alt="profile" height={40} width={40}></Image>
+      </div>
+    </header>
+  );
+}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { usePathname } from "next/navigation"; // Import usePathname
-import { FaUser, FaClipboardList } from "react-icons/fa";
-import Image from "next/image";
-import BackupIcon from "../resources/images/icons/clarity_backup-line.png";
+import { useState } from 'react';
+import { usePathname } from 'next/navigation'; // Import usePathname
+import { FaUser, FaClipboardList } from 'react-icons/fa';
+import Image from 'next/image';
+import BackupIcon from '../resources/images/icons/clarity_backup-line.png';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -15,15 +15,15 @@ const Sidebar = () => {
   };
 
   const menuItems = [
-    { label: "Dashboard", icon: <FaUser />, path: "/" },
-    { label: "Employee", icon: <FaUser />, path: "/employee" },
-    { label: "Project", icon: <FaClipboardList />, path: "/project" },
+    { label: 'Dashboard', icon: <FaUser />, path: '/' },
+    { label: 'Employee', icon: <FaUser />, path: '/employee' },
+    { label: 'Project', icon: <FaClipboardList />, path: '/project' }
   ];
 
   return (
     <div
-      className={`h-screen bg-[#F4F5FA] text-[#212F3F] flex flex-col ${
-        isOpen ? "w-64" : "w-16"
+      className={`flex h-screen flex-col bg-[#F4F5FA] text-[#212F3F] ${
+        isOpen ? 'w-64' : 'w-16'
       } transition-all duration-300`}
     >
       {/* Header */}
@@ -40,13 +40,13 @@ const Sidebar = () => {
       <nav className="flex-1">
         <ul>
           {menuItems.map((item, index) => (
-            <li key={index} className="pt-1 pb-1">
+            <li key={index} className="pb-1 pt-1">
               <a
                 href={item.path}
-                className={`flex items-center space-x-2 pt-3 pb-3 pl-6 rounded-tr-full rounded-br-full ${
+                className={`flex items-center space-x-2 rounded-br-full rounded-tr-full pb-3 pl-6 pt-3 ${
                   pathname === item.path
-                    ? "bg-red-600 text-white"
-                    : "hover:bg-red-600 hover:text-white"
+                    ? 'bg-red-600 text-white'
+                    : 'hover:bg-red-600 hover:text-white'
                 }`}
               >
                 <span>{item.icon}</span>
