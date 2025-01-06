cd764f5f9c0a124b3256ae71c9d31cc9e1501b84
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2025-01-06 14:23:28 +0700

    add login

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,6 @@
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const DATABASE_URL = process.env.DATABASE_URL!;
+const SITE_URL = process.env.SITE_URL || 'http://localhost:3000/';
 
-export { SUPABASE_URL, SUPABASE_KEY, DATABASE_URL };
+export { SUPABASE_URL, SUPABASE_KEY, DATABASE_URL, SITE_URL };
